2c521459a01e5d9e2201d220cb0ac44218398746
AptFox <AptFox@users.noreply.github.com>
2019-03-27 14:09:24 -0400

    Updated write-up, added comments

--- src/main/java/worksample/FoodTruckFinder/service/FoodTruckFinderService.java
+++ src/main/java/worksample/FoodTruckFinder/service/FoodTruckFinderService.java
@@ -23,12 +23,27 @@ public class FoodTruckFinderService {
 	public FoodTruckFinderService() {
 		consumer = Soda2Consumer.newConsumer(API_BASE_URL);
 	}
-
+	
+	
+	/**
+	 * Returns a ConditionalExpression to be used in a SoqlQuery
+	 * 
+	 * @param expression   a string in the format: "command=%s" where '%s' is the value to be plugged into the command
+	 * @param value        
+	 * @return             A ConditionalExpression
+	 */
 	private ConditionalExpression prepareExpression(String expression, String value) {
 		String formattedQuery = String.format(expression, value);
 		return new ConditionalExpression(formattedQuery);
 	}
 	
+	/**
+	 * @param offset
+	 * @param pageSize
+	 * @param cal
+	 * @return
+	 * @throws LongRunningQueryException
+	 */
 	public List<FoodTruckDTO> getPage(Integer offset, Integer pageSize, Calendar cal) throws LongRunningQueryException{
 		List<FoodTruckDTO> foodTrucks = new ArrayList<FoodTruckDTO>(); 
 		Integer currentHour = cal.get(Calendar.HOUR_OF_DAY); //24H format
@@ -42,7 +57,7 @@ public class FoodTruckFinderService {
 			expList.add(dayOrderEqualsCurrentDay);
 			expList.add(start24IsLessThanCurrentHour);
 			expList.add(end24IsGreaterThanCurrentHour);
-			Expression combinedWhereClause = new CompositeExpression(expList);
+			Expression combinedWhereClause = new CompositeExpression(expList); //Combines all generated expressions using 'AND' 
 			SoqlQuery foodTrucksOpenNowQuery = new SoqlQueryBuilder()
 			        .addSelectPhrase(API_SOQL_FOOD_TRUCK_NAME)
 			        .addSelectPhrase(API_SOQL_FOOD_TRUCK_ADDR)
@@ -55,8 +70,8 @@ public class FoodTruckFinderService {
 			        .setOffset(offset)
 			        .build();
 		
-			foodTrucks = consumer.query(API_RESOURCE_ID,
-										foodTrucksOpenNowQuery,
+			foodTrucks = consumer.query(API_RESOURCE_ID,        //Calls out to API and parses response into a list of FoodTruckDTO objects
+										foodTrucksOpenNowQuery, // JSON mapping done via Jackson library
 								        FoodTruckDTO.LIST_TYPE);
 		} 
 		catch (SodaError e) {
